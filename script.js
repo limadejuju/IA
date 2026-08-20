d3ac2f88253404952b4a5c13991fd1ef28909039
limadejuju <lima.juliano30@escola.pr.gov.br>
2026-08-20 08:45:57 -0300

    11

--- script.js
+++ script.js
@@ -82,12 +82,17 @@ let perguntaAtual;
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas()
 }
 
 function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
     const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa;
+    botaoAlternativas.AddEventListener("click", function(){
+      atual++;
+      mostraPergunta();
+    })
     caixaAlternativas.appendChild(botaoAlternativas);
   }
 }
